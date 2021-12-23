54f9f9bd3143eba188241cd25bca054acf86bfac
Mauricio Garavaglia <garavagliamauricio@gmail.com>
2021-12-22 23:55:24 -0300

    Personal config and colorscheme

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -62,6 +62,7 @@ return packer.startup(function(use)
   -- Colorschemes
   -- use "lunarvim/colorschemes" -- A bunch of colorschemes you can try out
   use "lunarvim/darkplus.nvim"
+  use 'thedenisnikulin/vim-cyberpunk'
 
   -- cmp plugins
   use "hrsh7th/nvim-cmp" -- The completion plugin
@@ -70,6 +71,7 @@ return packer.startup(function(use)
   use "hrsh7th/cmp-cmdline" -- cmdline completions
   use "saadparwaiz1/cmp_luasnip" -- snippet completions
   use "hrsh7th/cmp-nvim-lsp"
+  use "hrsh7th/cmp-nvim-lua"
 
   -- snippets
   use "L3MON4D3/LuaSnip" --snippet engine
